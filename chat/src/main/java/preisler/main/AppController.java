6f894fa0a8d0460d281d09b2b3fda18e2128e799
Preisler <preisler.andras@gmail.com>
2023-02-02 09:21:43 +0100

    login v0.1

--- chat/src/main/java/preisler/main/AppController.java
+++ chat/src/main/java/preisler/main/AppController.java
@@ -1,16 +1,36 @@
 package preisler.main;
 
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class AppController {
-    @RequestMapping(value="/")
+    @RequestMapping(value="/", method = RequestMethod.GET)
     public ModelAndView getIndexPage() {
         ModelAndView mav = new ModelAndView("index");
         mav.addObject("text", "Message of the day");
         return mav;
     }
+
+    @RequestMapping(value = "/login", method = RequestMethod.POST)
+    public String login(@RequestBody String temp, HttpServletResponse response){
+
+        String username = getData(temp, 0);
+        String password = getData(temp, 1);
+        String op = getData(temp, 2);
+
+        String query = ""
+
+        //Redirecting the user to the /main page
+        response.setStatus(HttpServletResponse.SC_MOVED_PERMANENTLY);
+        response.setHeader("Location", "/main");
+        return "redirect:/main";
+
+    }
+
+    private String getData(String data, int id) {return data.split("&")[id].split("=")[1];}
+
 
 }
